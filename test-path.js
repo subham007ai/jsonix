7ead7a5c8687d5f0f43fccf871590c1c27fcc122
Subham Sarangi <subhamsadangi9@gmail.com>
2026-03-12 09:34:16 +0530

    chore: remove agent files and debug outputs

--- test-path.js
+++
@@ -1,7 +0,0 @@
-const { testJSONPath } = require("./lib/json-utils.js");
-const json = `{"company": "Anthropic", "teams": [{"name": "Research", "lead": "Dario", "members": ["Alice", "Bob"], "budget": 5000000}]}`;
-
-let res = testJSONPath(json, "$.company");
-console.log(res);
-res = testJSONPath(json, "$.teams[0].name");
-console.log(res);
